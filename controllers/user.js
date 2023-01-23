4220fa1d4bc6143e690a9bcde65925447043e334
Almir Hodžić <hdzcalmir@gmail.com>
2023-01-23 14:31:26 +0100

    profile

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,22 @@
+import { db } from "../connect.js";
+import jwt from "jsonwebtoken";
+
 // sve operacije vezane za usera ce se desiti u ovom 
 
 // ovo je endpoint koji poziva funkciju koja izvrsava nesto u user controller
 export const getUser = (req, res) => {
     //TODO
-    res.send('useri rade!');
+    // res.send('useri rade!');
+    // getamo id iz linka
+    const userId = req.params.userId;
+    const q = "SELECT * FROM users WHERE id = ?";
+
+    db.query(q, [userId], (err, data) => {
+        // za ispis errora
+        if (err) return res.status(json).json();
+
+        const { password, ...info } = data[0];
+        return res.json(info);
+
+    })
 }
